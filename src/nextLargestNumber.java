140d9b6f1ace1a1b2e5964d2026b3eaf58ec7a26
Harsh Jaiswal <jaiswal2.hj2001@gmail.com>
2025-07-17 11:16:43 +0530

    adding login to next greater element by myself

--- src/nextLargestNumber.java
+++ src/nextLargestNumber.java
@@ -4,19 +4,33 @@ import java.util.Stack;
 public class nextLargestNumber {
     public static void main(String[] args) {
         int[] a = {6, 0, 8, 1, 3};
+        int b[]= new int[a.length];
         int n = 5;
         int nextElement;
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = a.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && a[i] >= stack.peek()) {
-                stack.pop();
-            }
-            nextElement = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(a[i]);
-            a[i]=nextElement;
+//        for (int i = a.length - 1; i >= 0; i--) {
+//            while (!stack.isEmpty() && a[i] >= stack.peek()) {
+//                stack.pop();
+//            }
+//            nextElement = stack.isEmpty() ? -1 : stack.peek();
+//            stack.push(a[i]);
+//            a[i]=nextElement;
+//        }
+        for (int i=0;i<n;i++){
+            b[i]=-1;
         }
 
-        System.out.print(Arrays.toString(a));
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && a[i]>=stack.peek()){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                b[i]=stack.peek();
+            }
+            stack.push(a[i]);
+        }
+
+        System.out.print(Arrays.toString(b));
     }
 }
